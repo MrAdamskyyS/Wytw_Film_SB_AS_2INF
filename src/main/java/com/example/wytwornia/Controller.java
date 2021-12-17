1f5aafb768b6e8c3f8d8ce8e8d295a2f9c633933
sztywnygit <addav.addav@gmail.com>
2021-12-17 23:03:12 +0100

    more functionality to menu
    -klikniecie filmy odkrywa tabelke
    -dodano koszyk.fxml + oprogramowano przycisk anuluj

--- src/main/java/com/example/wytwornia/Controller.java
+++ src/main/java/com/example/wytwornia/Controller.java
@@ -4,20 +4,56 @@ package com.example.wytwornia;
 
 
 
+
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
+import javafx.stage.Window;
+
 
 import java.io.IOException;
 
-public class Controller {
+public class Controller  {
+    public ScrollPane ScrollPaneFilmy;
+    Button anulujButton;
+
+
+    public Controller() throws IOException {
+
+    }
+
+
     @FXML
-    private Label labelfajny;
+    public void btnFilmyonAction() {
+        ScrollPaneFilmy.setVisible(true);
+
+
+    }
+    @FXML
+    public void btnAnulujOnAction(ActionEvent event) {
+        Window window =   ((Node)(event.getSource())).getScene().getWindow();
+        if (window instanceof Stage){
+            ((Stage) window).close();
+        }
+    }
+    @FXML
+    public void openKoszyk() throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("koszyk.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
 
     @FXML
     public void changeSceneToMain(ActionEvent event) throws IOException {
--- src/main/java/com/example/wytwornia/Main.java
+++ src/main/java/com/example/wytwornia/Main.java
@@ -9,7 +9,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.SQLException;
-
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException, SQLException {
@@ -19,8 +18,10 @@ public class Main extends Application {
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.setResizable(false);
-        DatabaseConnection connection = new DatabaseConnection();
-        connection.query();
+
+
+  //  DatabaseConnection connection = new DatabaseConnection();
+//connection.query();
      //   connection.connect();
             stage.show();
 
